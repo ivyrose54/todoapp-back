f617f6bcbf1639182b1208b0cd56c6196f1cd51a
ivyrose54 <bual.ivyrose@gmail.com>
2025-04-17 01:55:32 +0800

    first commit

---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class TodoBase(BaseModel):
+    title: str
+    completed: bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoUpdate(TodoBase):
+    pass
+
+class TodoOut(TodoBase):
+    id: int
+    class Config:
+        orm_mode = True
